e34a31754be38460a65fec1b8b0ddd9458cbef44
cristtian177 <cristtian177@gmail.com>
2023-10-22 17:52:20 -0500

    [ADD] gitignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const session = require("express-session");
 const app = express();
@@ -8,12 +8,12 @@ require("./auth");
 
 app.use(express.json());
 
-
 function isLoggedIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
 }
 
-app.use( session({
+app.use(
+  session({
     secret: "mysecret",
     resave: false,
     saveUninitialized: true,
@@ -21,32 +21,21 @@ app.use( session({
   })
 );
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["email", "profile"],
-  })
-);
+app.get("/auth/google", passport.authenticate("google", { scope: ["email", "profile"] }));
 
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    successRedirect: "/auth/protec",
-    failureRedirect: "/auth/google/failure",
-  })
-);
+app.get("/auth/google/callback", passport.authenticate("google", {
+  successRedirect: "/auth/proteced",
+  failureRedirect: "/auth/google/failure",
+}));
 
-app.get("/auth/protec", isLoggedIn, (req, res) => {
-  //let name = req.user;
-  //res.send(`Hello ${name}`);
-  // Accede al objeto del usuario directamente
-  let user = req.user;
+app.get("/auth/proteced", isLoggedIn, (req, res) => {
+  const user = req.user;
+  // Genera un JWT con la información del usuario del auth
+  const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
-  // Convierte el objeto en una cadena JSON y envíala
-  res.send(user);
-
+  res.json(token);
 });
 
 app.get("/auth/google/failure", isLoggedIn, (req, res) => {
@@ -54,9 +43,8 @@ app.get("/auth/google/failure", isLoggedIn, (req, res) => {
 });
 
 app.get("/auth/logout", isLoggedIn, (req, res) => {
-  req.session.destroy()
-  res.send('see you again')
+  req.session.destroy();
+  res.send('See you again');
 });
-
 
 app.listen(5000, console.log("Listening on port 5000"));
