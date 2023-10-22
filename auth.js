d57d1acbed5b60362449020e5e4efb59fc30f17c
cristtian177 <cristtian177@gmail.com>
2023-10-22 17:52:20 -0500

    [ADD] gitignore

--- auth.js
+++ auth.js
@@ -2,6 +2,11 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 require("dotenv").config();
 
+const redis = require("redis");
+const client = redis.createClient({
+  url: "redis://localhost:6379",
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -12,11 +17,37 @@ passport.use(
     },
     function (request, accessToken, refreshToken, profile, done) {
       profile = profile._json;
+      //console.log(profile);
+
+      profile = [
+        {
+          id_user: profile.sub,
+          name: profile.name,
+          picture: profile.picture,
+          email: profile.email,
+          isAdmin: null,
+        },
+      ];
+      //console.log(profile);
+
+      // almacena en redis
+      const userSessionKey = `user:${profile}`;
+      const expirationTime = 3600000; //ms
+      client.setEx(
+        userSessionKey,
+        JSON.stringify(profile),
+        "PX",
+        expirationTime
+      );
+
       done(null, profile);
-      console.log(profile);
     }
   )
 );
+
+client.on("error", function (err) {
+  console.error("Error de Redis: " + err);
+});
 
 passport.serializeUser((user, done) => {
   done(null, user);
